ace1eafbb73f784281d2b5d74a8727d0c52c9502
Ruben Cancho <canchete@gmail.com>
2016-10-29 21:50:15 +0200

    Añadido random

--- lib/genpreg.rb
+++ lib/genpreg.rb
@@ -2,14 +2,40 @@
 
 require './lib/ipgen.rb'
 
-def generar_pregunta(pregunta)
-  puts "::#{pregunta[:titulo]}::#{pregunta[:descripcion]} {" 
+# TODO: permitir generar tipos GIFT o CLOZE
+# Pensar si hacer clases de generación o un if de momento...
+$xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<quiz>
+<question type="cloze"><name><text>Direcciones inicial y final</text></name><questiontext format="html"><text>
+<![CDATA[*]]>
+</text>
+</questiontext>
+<generalfeedback format="html">
+<text/>
+</generalfeedback><penalty>0.3333333</penalty><hidden>0</hidden>
+</question>
+</quiz>
+'''
 
-  pregunta[:ipgen].each do |tipo_ip,cantidad|
+
+def generar_pregunta(pregunta)
+  output = "::#{pregunta[:titulo]}::#{pregunta[:descripcion]} {" 
+
+  lista = []
+
+  pregunta[:ipgen].each do |tipo,cantidad|
     (1..cantidad).each do |n|
-      puts pregunta[:codigo].call(tipo_ip)
+      lista << tipo
     end
   end
 
-  puts "}"
+  lista = lista.shuffle
+
+  lista.each do |tipo|
+      output = output + "\n" + pregunta[:codigo].call(tipo)
+  end
+
+  output = output + "\n}"
+
+  puts output
 end
