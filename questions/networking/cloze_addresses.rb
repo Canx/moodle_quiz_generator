586223ab0cfec6f8251021038bd36f96ff4ecc06
Ruben Cancho <canchete@gmail.com>
2016-11-17 12:45:30 +0100

    Corrected question

--- questions/networking/cloze_addresses.rb
+++ questions/networking/cloze_addresses.rb
@@ -1,25 +1,21 @@
 question = {
   :type => :cloze,
-  :name => "Subnetting, red más adecuada",
+  :name => "Información de la red",
   :description => "Indica cual es la dirección inicial y final de las siguientes redes:",
   :num => 1,
   :generators => {
     :ip => {
-       "ip" => {
-               {mask: 28} => 1,
-               {mask: 16} => 1,
-               {mask: 24} => 1,
-               {mask: 26} => 1 }}
+       "ip" => { {:mask => (16..28)} => 4}}
   },
   :answers => {
     :pre => "    <br /><pre>\n
-    <table><tr><th>Hosts</th><th>Dirección en rango</th><th>Máscara necesaria</th><th>Dirección de red</th><th>Ip inicial red</th><th>Ip final</th>",
+    <table><tr><th>Dirección IP</th><th>Hosts</th><th>Máscara</th><th>Ip inicial</th><th>Ip final</th>",
     :answer => "<tr>
-                  <td><%= ip.size - 2 %> hosts</td>
                   <td><%= ip.to_s %></td>
-                  <td>{1:SHORTANSWER:%100%#<%= ip.mask %>}</td>
-                  <td>{1:SHORTANSWER:%100%#<%= ip.initial %>}</td>
-                  <td>{1:SHORTANSWER:%100%#<%= ip.final %>}</td>
+                  <td>{1:SHORTANSWER:%100%<%= ip.size - 2 %>} hosts</td>
+                  <td>{1:SHORTANSWER:%100%<%= ip.netmask %>}</td>
+                  <td>{1:SHORTANSWER:%100%<%= ip.initial %>}</td>
+                  <td>{1:SHORTANSWER:%100%<%= ip.final %>}</td>
                 </tr>",
     :post => "    </table></pre>"}
 }
