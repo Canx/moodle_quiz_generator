a39570ea9f2cbef7f8a55460cec1d631758a1ced
Ruben Cancho <canchete@gmail.com>
2017-10-04 12:03:31 +0200

    Added new question about speed units.

---
+++ questions/units/cloze_speed_units.rb
@@ -0,0 +1,17 @@
+question = {
+  :type => :cloze,
+  :name => "Unidades de velocidad",
+  :description => "Indica cuanto tiempo en segundos tardarías en descargar los siguientes archivos, dada la velocidad de descarga indicada:",
+  :num => 10,
+  :generators => {
+    :random => {
+      :file_size => {{ :range => (5..2000) } => 1},
+      :speed =>     {{ :elements => [6,12,20,50,100] } => 1}
+    }
+  },
+  :answers => {
+    :pre => "<table><tr><th>Archivo (MiB)</th><th>Velocidad descarga (Mbps)</th><th>Tiempo de descarga (s)</th>",
+    :answer => "<tr><td><%= file_size %></td><td><%= speed %></td><td>{1:NUMERICAL:=<%= ((file_size*1024*1024)/(1000*1000)).fdiv(speed) %>:0.1}</td></tr>",
+    :post => "</table>"
+  }
+}
